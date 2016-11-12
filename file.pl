085ddd21d5874ea5064cd0d946493ef30c75c970
Avis <orlov.avis@yandex.com>
2016-11-12 22:12:36 +0300

    Тестовый файл

---
+++ file.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+use strict;
+
+=head operator sort
+my @sorted = sort qw(Здесь текст сортированный по алфавиту);
+print "@sorted";
+=cut
+
+=head operator reverse
+my @reverse = reverse qw(Здесь текст сортированный в обратном порядке);
+print "@reverse";
+=cut
